a160d6b26f3d6c54f99726601739bfd2abc676b9
Munaa <munaa.tsetsegmaa@gmail.com>
2020-04-07 19:42:56 +0800

    weather app

---
+++ docs/precache-manifest.99504b532f48abc1e04f0405e0481512.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8488948227e7781586047835eaed3c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ffd55261e78f8b3946",
+    "url": "/static/css/main.f7ec8a4e.chunk.css"
+  },
+  {
+    "revision": "1c6640f74f5191159d8b",
+    "url": "/static/js/2.2eabd1d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2eabd1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ffd55261e78f8b3946",
+    "url": "/static/js/main.2dbc88d0.chunk.js"
+  },
+  {
+    "revision": "3e26213f0b416f82a474",
+    "url": "/static/js/runtime-main.3e5e7252.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
